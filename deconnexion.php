22d6bb4fa350d8388067a0b16c033479387c06c2
zineb bennani <zineb.bennanigabsi@gmail.com>
2023-08-16 14:00:00 +0100

    closes the PDO connection

--- deconnexion.php
+++ deconnexion.php
@@ -1,9 +1,12 @@
 <?php
+// Simple disconnect helper - closes the PDO connection (if any)
+// and prints a confirmation message.
 require_once 'connexion.php';
-try{$pdo=null;
-     echo'vous etes deconnecter';
-}catch(PDOException $e){
-    die('erreur'.$e->getMessage());
+try {
+    $pdo = null;
+    echo 'You have been disconnected';
+} catch (PDOException $e) {
+    die('error: ' . $e->getMessage());
 }
 
 ?>
