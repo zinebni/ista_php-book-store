3573b1d78d876b32ab3921f3df309664b5959cf1
zineb bennani <zineb.bennanigabsi@gmail.com>
2023-08-16 14:00:00 +0100

    Database connection helper

--- connexion.php
+++ connexion.php
@@ -1,10 +1,15 @@
 <?php
-    try{
-        $pdo= new PDO('mysql:host=localhost;dbname=biblio','root','');
-        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        
-    }catch(PDOException $e){
-        die('errer:'.$e->getMessage());
-    }
+// Database connection helper
+// Creates a PDO instance connected to the local MySQL 'biblio' database
+// Throws an exception on error and stops execution.
+try{
+    $pdo = new PDO('mysql:host=localhost;dbname=biblio','root','');
+    // Use exceptions for error handling so calling code can catch failures
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+} catch (PDOException $e) {
+    // If the connection fails, stop and show the error message
+    die('error: '.$e->getMessage());
+}
 
 ?>
